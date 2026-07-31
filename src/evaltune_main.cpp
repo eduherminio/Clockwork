2f8353707aeaf52725424d7144c7255614027d59
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2026-07-31 15:51:14 +0200

    Use only Lynx filtering over v5, without CW phase sampling
    Bench: 12904595

--- src/evaltune_main.cpp
+++ src/evaltune_main.cpp
@@ -39,7 +39,7 @@ int main() {
     std::vector<f64>      results;
 
     const std::vector<std::string> fenFiles = {
-      "/mnt/d/Clockwork-data/v5_25knpm_after_phase_filter.vf_shuffled-cw.epd",
+      "/mnt/d/Clockwork-data/v5_25knpm.vf_shuffled.epd",
     };
 
     const u32 thread_count = std::max<u32>(1, std::thread::hardware_concurrency());
